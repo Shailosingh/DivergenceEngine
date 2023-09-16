9bed8f19b24817e7578c9d585e674e659a976833
Shailendra Singh <computerface56@hotmail.com>
2023-09-15 23:01:08 -0400

    Changed up EntryPoint, so in debug mode, unhandled exceptions go straight to the debugger. However, in release mode, the exception info is actually popped up on a MessageBox for the user to see (without a debugger) and then the program will close.

--- DivergenceEngine/src/Application/EntryPoint.h
+++ DivergenceEngine/src/Application/EntryPoint.h
@@ -8,8 +8,59 @@
 //External function that shall setup the application
 extern DivergenceEngine::Application* DivergenceEngine::CreateApplication(LPWSTR lpCmdLine);
 
+//Different versions of entry point
+int WINAPI ReleaseEntryPoint(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow);
+int WINAPI DebugEntryPoint(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow);
+
 //Entry point of program
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	int returnCode = 0;
+
+	//Call the respective debug or release entry point
+#ifndef NDEBUG
+	DivergenceEngine::Logger::Log(L"DEBUG MODE");
+	returnCode = DebugEntryPoint(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+#else
+	DivergenceEngine::Logger::Log(L"RELEASE MODE");
+	returnCode = ReleaseEntryPoint(hInstance, hPrevInstance, lpCmdLine, nCmdShow);
+#endif
+
+	return returnCode;
+}
+
+int WINAPI DebugEntryPoint(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	DivergenceEngine::Logger::Log(L"Welcome to the Divergence Engine! Here we take the road less travelled by...");
+
+	//Retrieve the Application object from the user
+	DivergenceEngine::Application* application = DivergenceEngine::CreateApplication(lpCmdLine);
+
+	//Initialize the application (all windows and other info)
+	application->Initialize();
+
+	//Pump message loop
+	MSG incomingMessage = { 0 };
+	while (incomingMessage.message != WM_QUIT)
+	{
+		if (PeekMessage(&incomingMessage, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&incomingMessage);
+			DispatchMessage(&incomingMessage);
+		}
+
+		else
+		{
+			application->UpdateAndDrawAllWindows();
+		}
+	}
+
+	//Cleanup 
+	delete application;
+	return (int)incomingMessage.wParam;
+}
+
+int WINAPI ReleaseEntryPoint(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
 {
 	DivergenceEngine::Logger::Log(L"Welcome to the Divergence Engine! Here we take the road less travelled by...");
 
@@ -52,7 +103,7 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 		MessageBoxA(NULL, e.what(), "Error In Message Loop", MB_OK | MB_ICONERROR);
 		return -1;
 	}
-	
+
 	//Cleanup and throw a dialog message for any unhandled exceptiond during cleanup. Terminate immediately.
 	try
 	{
@@ -62,6 +113,6 @@ int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, 
 	{
 		MessageBoxA(NULL, e.what(), "Error On Exit Of Game", MB_OK | MB_ICONERROR);
 	}
-	
+
 	return (int)incomingMessage.wParam;
 }
