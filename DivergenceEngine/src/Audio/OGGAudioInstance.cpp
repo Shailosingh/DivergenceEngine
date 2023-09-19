69cb1d9d5c2a67067e7d638d1e67e3df9a0a4dd3
Shailosingh <computerface56@hotmail.com>
2023-09-19 16:03:36 -0400

    Backing up code

--- DivergenceEngine/src/Audio/OGGAudioInstance.cpp
+++ DivergenceEngine/src/Audio/OGGAudioInstance.cpp
@@ -1,5 +1,43 @@
 #include "Audio/IAudioInstance.h"
 #include "Audio/OGGAudioInstance.h"
+#include "Logger/Logger.h"
+#include <stdexcept>
+#include <stdio.h>
 
 //https://xiph.org/vorbis/doc/vorbisfile/overview.html
 //https://github.com/edubart/minivorbis
+
+namespace DivergenceEngine
+{
+	OGGAudioInstance::OGGAudioInstance(DirectX::AudioEngine* engine, std::wstring filePath, uint8_t initialPlaybackSpeedMultiplier, float initialVolume)
+	{
+		//Handle invalid parameters
+		if (engine == nullptr)
+		{
+			throw std::invalid_argument("OGGAudioInstance::OGGAudioInstance() - engine cannot be nullptr");
+		}
+		EnginePointer = engine;
+
+		if (initialPlaybackSpeedMultiplier == 0)
+		{
+			throw std::invalid_argument("OGGAudioInstance::OGGAudioInstance() - initialPlaybackSpeedMultiplier cannot be 0");
+		}
+		PlaybackSpeedMultiplier = initialPlaybackSpeedMultiplier;
+
+		if (initialVolume > 1 || initialVolume < 0)
+		{
+			throw std::invalid_argument("OGGAudioInstance::OGGAudioInstance() - initialVolume must be between 0 and 1");
+		}
+
+		//Try to open the file C style
+		FILE* fileObject = _wfopen(filePath.c_str(), L"r");
+		if (fileObject == nullptr)
+		{
+
+		}
+	}
+
+	OGGAudioInstance::~OGGAudioInstance()
+	{
+	}
+}
--- DivergenceEngine/src/Audio/OGGAudioInstance.h
+++ DivergenceEngine/src/Audio/OGGAudioInstance.h
@@ -1,3 +1,36 @@
 #pragma once
 #include "Audio/IAudioInstance.h"
 #include "vorbis/vorbisfile.h"
+
+namespace DivergenceEngine
+{
+	class OGGAudioInstance : public IAudioInstance
+	{
+	private:
+		//General Datafields
+		DirectX::AudioEngine* EnginePointer;
+		std::unique_ptr<DirectX::DynamicSoundEffectInstance> SoundEffectInstance;
+		OggVorbis_File VorbisFileObject;
+		uint8_t PlaybackSpeedMultiplier;
+		bool IsLoop;
+
+		//Buffer variables
+		const int32_t MAX_BUFFERS = 5;
+
+		//Buffer functions
+		void BufferNeeded(DirectX::DynamicSoundEffectInstance* instance);
+
+	public:
+		//Constructors and Destructors
+		OGGAudioInstance(DirectX::AudioEngine* engine, std::wstring filePath, uint8_t initialPlaybackSpeedMultiplier = 1, float initialVolume = 1);
+		~OGGAudioInstance();
+
+		//Overriden functions
+		void Play(bool isLoop = true) override;
+		void Stop() override;
+		void Pause() override;
+		void Resume() override;
+		void SetVolume(float newVolume) override;
+		void SetPlaybackSpeedMultiplier(uint8_t newPlaybackSpeedMultiplier) override;
+	};
+}
